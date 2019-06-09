3b190b8689acdade821aaae2cfd0513fe347affc
Aidan Stritch <aidan.stritch@gmail.com>
2019-06-09 13:31:27 +0000

    update map details and start using places API

--- scripts/map.js
+++ scripts/map.js
@@ -1,32 +1,82 @@
-//AIzaSyAMKEXR1MPX21maPjxSSX1zAweVa6JiXPw -- API key
+//AIzaSyAxQwRApZQq4D_0iTu38MwdxUsiKrq4zu8 -- API key
 
-var map;
+//        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxQwRApZQq4D_0iTu38MwdxUsiKrq4zu8&libraries=places&callback=callback"></script>
+
+
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
-        center: {lat: 46.619261, lng: -33.134766}
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        //change this center to be the one we click? hide map until selection made? 
+        center: {lat: 53.3498, lng: -6.2603}
     });
     
-    var markerLabels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var places = [
-        { lat: 53.3498, lng: -6.2603 },
-        { lat: 45.4642, lng: 9.1900 },
-        { lat: 48.8566, lng: 2.3522 },
-        { lat: 40.7128, lng: -74.0060 },
-        { lat: 52.5200, lng: 13.4050 }
-    ];
-
-    var markers = places.map(function(place, i) {
-        return new google.maps.Marker({
-            position: place,
-            label: markerLabels[i % markerLabels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+   
+    //request results to be added to the map
+    
+ /*   
+    if (getElementById ('cityName') == 'a') {
+        
+        var request = {
+        location: center,
+        radius: 8000, 
+        types: ['bar', 'restaurant', 'night_club', 'food']
+        };
+    }
+    
+    if (getElementById ('cityName') == 'b') {
+        var request = {
+        location: center,
+        radius: 8000, 
+        types: ['art_gallery', 'aquarium', 'zoo', 'stadium', 'museum', 'park', 'casino', 'amusement_park', 'point_of_interest']
+        };
+    }
+    
+    if (getElementById ('cityName') == 'c') {
+        var request = {
+        location: center,
+        radius: 8000, 
+        types: ['campground', 'lodging', 'rv_park', 'room', 'premise']
+        };
+    }
+*/
+    //delete this later
+    
+    var request = {
+        location: center,
+        radius: 8047, 
+        types: ['cafe']
+        };
+    
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 }
+
+/*
+
+function selectCity() {
+            var city = document.getElementById('cityName');
+            var autocomplete = new google.maps.place.Autocomplete(city);
+*/
+
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++){
+            createMarker(results[i]);
+        }
+    } 
+}
+
+function createMarker(place){
+    var marker = new google.maps.Marker({
+        map: center,
+        position: place.geometry.location,
+        });
+        
+
+}
+
 
 /*
 53.3498 , -6.2603 - dublin
@@ -35,3 +85,4 @@ function initMap() {
 40.7128, -74.0060 - new york
 52.5200, -13.4050 - berlin
 */
+
