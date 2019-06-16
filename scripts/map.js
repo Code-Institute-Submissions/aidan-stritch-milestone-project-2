662581124cf7f741cb57c11fdec31dac4d8c53c5
Aidan Stritch <aidan.stritch@gmail.com>
2019-06-16 21:12:51 +0000

    added clear button functionality to clear markers on map

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,9 @@
 var map;
 var infowindow;
 var center = {lat: 53.3498, lng: -6.2603};
-var updatedType;
+var updatedType =[];
+var marker;
+var marker2 = [];
 
 function initMap() {
     
@@ -14,14 +16,11 @@ function initMap() {
     });
     
     searchSelectCity();
-    buttonSelectSearchType();
     buttonSelectCity();
+    buttonSelectSearchType();
     
-    
-    
-    
-   
 }
+
 function searchSelectCity() {
     var typeCity = document.getElementById('cityName');
     var autocomplete = new google.maps.places.Autocomplete(typeCity);
@@ -33,64 +32,72 @@ function searchSelectCity() {
     
     cityForm.addEventListener("submit", function() {
         updateLatLng = new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng());
-        map.setCenter(updateLatLng);
         map.setZoom(13);
-        requestLocations(updateLatLng);
+        center=updateLatLng;
+        requestLocations();
     });
 }
+
 function buttonSelectSearchType(){
     document.getElementById('attraction').onclick = function() {
         updatedType = ['art_gallery', 'aquarium', 'zoo', 'stadium', 'museum', 'park', 'casino', 'amusement_park', 'point_of_interest'];
+        requestLocations();
     };
     document.getElementById('accom').onclick = function() {
         updatedType = ['campground', 'lodging', 'rv_park', 'room', 'premise'];
+        requestLocations();
     };
     document.getElementById('bar').onclick = function() {
         updatedType = ['bar', 'restaurant', 'night_club', 'food'];
+        requestLocations();
     };
     document.getElementById('all').onclick = function() {
-        updatedType = ['bar', 'restaurant', 'night_club', 'food','campground', 'lodging', 'rv_park', 'room', 'premise', 'art_gallery', 
-        'aquarium', 'zoo', 'stadium', 'museum', 'park', 'casino', 'amusement_park', 'point_of_interest'];
+        updatedType = ['bar', 'restaurant', 'night_club', 'food','campground', 'lodging', 'rv_park', 'room', 'premise', 'art_gallery', 'aquarium', 'zoo', 'stadium', 'museum', 'park', 'casino', 'amusement_park', 'point_of_interest'];
+        requestLocations();
     };
 }
-
 
 function buttonSelectCity(){
     document.getElementById('dublin').onclick = function() {
         center = {lat: 53.3498, lng: -6.2603};
+        requestLocations();
     };
     document.getElementById('milan').onclick = function() {
         center = {lat: 45.4642, lng: 9.1900};
+        requestLocations();
     };
     document.getElementById('paris').onclick = function() {
         center = {lat: 48.8566, lng: 2.3522};
+        requestLocations();
     };
     document.getElementById('newYork').onclick = function() {
         center = {lat: 40.7128, lng: -74.0060};
+        requestLocations();
     };
     document.getElementById('berlin').onclick = function() {
         center = {lat: 52.5200, lng: 13.4050};
+        requestLocations();
     };
 }
 
+
+
 function requestLocations(){
     map.setCenter(center);
-    //use these to 'get' the session variables.. then tell the request to use them for the search
-    //var newLocation;
-    //var newType;
-    
+
         var request = {
         location: center,
         radius: 8047, 
-        //types: ['bar', 'restaurant', 'night_club', 'food']
         types: updatedType
         };
-    
+        
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch(request, callback);
+
 }
 
 function callback(results, status) {
+    
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++){
             createMarker(results[i]);
@@ -99,24 +106,24 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
+    marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
     });
+    
+    marker2.push(marker);
 
     google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(`<h1>Example HTML, city name: ${place.name}</h1>`);
+      infowindow.setContent(`<h2>${place.name}</h2> <h3>Rating: ${place.reviews}</h3> <h3>Opening Hours</h3> ${place.opening_hours}`);
       infowindow.open(map, this);
     });
   }
 
-function showResults(){
-    requestLocations();
-}
-
 function clearResults(){
-    updatedType = [];
-    createMarker(updatedType);
+
+    for(i=0; i<marker2.length; i++){
+        marker2[i].setMap(null);
+    }
 
 }
 
