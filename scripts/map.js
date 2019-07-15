28160cd7b4ba5b80edb726f0b5039146d18457e4
Aidan Stritch <aidan.stritch@gmail.com>
2019-07-15 22:46:34 +0000

    created extra buttons and rearranged layout for buttons

--- scripts/map.js
+++ scripts/map.js
@@ -4,7 +4,7 @@ var infowindow;
 var center;
 var updatedType = [];
 var marker = [];
-var markersClear =[];
+var markersClear = [];
 var place = "";
 var searchResults = [];
 /* here we are setting up the required variables for pagination */
@@ -16,6 +16,7 @@ moreResults.onclick = function() {
 
 /* this function initiates the map element and displays it*/
 function initMap() {
+    //the map and results divs are shown to the user depending on what button has been clicked
 
     infowindow = new google.maps.InfoWindow();
 
@@ -23,11 +24,13 @@ function initMap() {
         zoom: 13,
         center: center
     });
-    
+
     /* this area calls the functions required to set the city and search type variables */
     searchSelectCity();
     buttonSelectCity();
     buttonSelectSearchType();
+
+
 }
 
 /* this function handles the text input and the autocomplete functionality so that the user can select a specific city */
@@ -43,7 +46,7 @@ function searchSelectCity() {
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         place = autocomplete.getPlace();
     });
-    
+
     /* when the submit button is clicked in the cityForm, the map center is changed to this new location and the values are set using the DOM */
     cityForm.addEventListener("submit", function() {
         if (place !== "") {
@@ -86,7 +89,7 @@ function buttonSelectSearchType() {
         document.getElementById('textSearchType').innerHTML = "Attractions, Accomodation, Bars & Restaurants";
         document.getElementById('search_type').value = "Attractions, Accomodation, Bars & Restaurants";
         updatedType = ['bar', 'restaurant', 'night_club', 'food', 'art_gallery', 'aquarium', 'zoo', 'stadium', 'museum', 'park', 'casino', 'amusement_park', 'point_of_interest', 'campground', 'lodging', 'rv_park', 'room', 'premise'];
-        
+
         $('.step-two-tick').show();
     };
 }
@@ -149,6 +152,9 @@ function requestLocations() {
     else {
         alert("Please select a city AND a search type before searching");
     }
+
+
+
 }
 
 /* this function takes in the results of the request and adds markers to the map and 
@@ -156,7 +162,7 @@ places the text results using createMarker() and resultsTextDisplay() respective
 function callback(results, status, pagination) {
     searchResults = [];
     searchResults = results;
-    
+
     console.log(status);
 
     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -170,15 +176,13 @@ function callback(results, status, pagination) {
             pagination.nextPage();
         };
     }
-    
-    if(searchResults.length == 0){
+
+    if (searchResults.length == 0) {
         alert("There are no results for the currrent selection");
         $('#map').hide();
         $('.results-area').hide();
         $('.email-form-area').hide();
-
     }
-    
 }
 
 /* this function adds the markers to the map element and adds an info window to show information for each result when the icon is clicked */
@@ -195,12 +199,10 @@ function createMarker(place) {
         infowindow.open(map, this);
     });
 
-    //the map and results divs are shown to the user
-    $('#map').show();
-    $('.results-area').show();
-    $('.email-form-area').show();
+    //the step 1 and 2 divs are hidden once a search has been made
     $('.select-city').hide();
     $('.narrow-results').hide();
+    $('#second-clear-button').show();
 
 }
 
@@ -212,7 +214,7 @@ function resultsTextDisplay(place) {
     service.getDetails({ placeId: newPlace }, function(place, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             if (place) {
-                document.getElementById('resultsRow').innerHTML += "<div class='row newResultsRow'><div class = 'col-md-4 newResultsCol'>" + "<h4 class='resultNames'>" + place.name + "</h4>" + "<p>" + "<b>" + "Rating: " + "</b>" + 
+                document.getElementById('resultsRow').innerHTML += "<div class='row newResultsRow'><div class = 'col-md-4 newResultsCol'>" + "<h4 class='resultNames'>" + place.name + "</h4>" + "<p>" + "<b>" + "Rating: " + "</b>" +
                     place.rating + " Stars" + "</p>" + "</div>" + "<div class = 'col-md-4'>" + "<p>" + place.formatted_address + "</p>" + "</div>" +
                     "<div class = 'col-md-4 newResultsCol'>" + "<p>" + "<b>" + "Contact: " + "</b>" + place.formatted_phone_number + "</p>" + "<p>" + "<b>" + "Web: " + "</b>" + place.website + "</p>" + "</p>" + "</div>" + "</div>";
             }
@@ -222,11 +224,11 @@ function resultsTextDisplay(place) {
 
 /* this function removes the map markers from the map element when the clear button is clicked*/
 function clearMap() {
-    
-        for (var i = 0; i < markersClear.length; i++) {
-            markersClear[i].setMap(null);
-        }
-        markersClear = [];
+
+    for (var i = 0; i < markersClear.length; i++) {
+        markersClear[i].setMap(null);
+    }
+    markersClear = [];
 }
 
 /* this function resets the input fields and values to their inital values and 
@@ -237,7 +239,9 @@ function clearResults() {
     document.getElementById('city_name').value = "None Selected";
     document.getElementById('search_type').value = "None Selected";
     document.getElementById('resultsRow').innerHTML = "";
-    document.getElementById('cityName').value = "Search by city name...";
+    $("#cityForm")[0].reset();
+    $('#second-clear-button').hide();
+    
     place = "";
     center = "";
     updatedType = [null];
@@ -245,13 +249,67 @@ function clearResults() {
 }
 
 
-//when the clearResults button is clicked - these divs and classes are hidden to the user
+
 $("#clearResults").click(function() {
+    //when the clearResults button is clicked - these divs and classes are hidden to the user
     $('#map').hide();
     $('.results-area').hide();
     $('.email-form-area').hide();
     $('.step-one-tick').hide();
     $('.step-two-tick').hide();
+    $('.step-three-tick').hide();
+    $('.step-four-tick').hide();
+    $('.step-five-tick').hide();
+
+    //these are shown to the user again
     $('.select-city').show();
     $('.narrow-results').show();
+    $('#showResults').show();
+    $('#showTextResults').show();
+    $('#sendEmail').show();
+
+});
+
+$('#showResults, #showTextResults, #sendEmail').click(function() {
+    if (document.getElementById('textCity').innerHTML != "None" && document.getElementById('textSearchType').innerHTML != "None") {
+        if (this.id == 'showResults') {
+
+            //when the showResults button is clicked, the map area is shown and results and email area divs are hidden tick is shown
+            $('.results-area').hide();
+            $('.email-form-area').hide();
+            $('.step-three-tick').show();
+            $('#map').show();
+
+            //when this button is clicked, the button is hidden from view to neaten view space and other buttons are made visible
+            $('#showResults').hide();
+            $('#showTextResults').show();
+            $('#sendEmail').show();
+        }
+
+
+        else if (this.id == 'showTextResults') {
+            //when the showTextResults button is clicked, the map and email area divs are hidden and the results area shown and tick is shown
+            $('#map').hide();
+            $('.email-form-area').hide();
+            $('.results-area').show();
+            $('.step-four-tick').show();
+
+            //when this button is clicked, the button is hidden from view to neaten view space and other buttons are made visible
+            $('#showResults').show();
+            $('#showTextResults').hide();
+            $('#sendEmail').show();
+        }
+        else if (this.id == 'sendEmail') {
+            //when the sendEmail button is clicked, the map and results area divs are hidden and the email area shown and tick is shown
+            $('#map').hide();
+            $('.results-area').hide();
+            $('.email-form-area').show();
+            $('.step-five-tick').show();
+
+            //when this button is clicked, the button is hidden from view to neaten view space and other buttons are made visible
+            $('#showResults').show();
+            $('#showTextResults').show();
+            $('#sendEmail').hide();
+        }
+    }
 });
